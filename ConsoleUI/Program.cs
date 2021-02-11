d80e27ec76608420c787f3936b17f44edefef100
Yunus Kaydan <ynskaydan@gmail.com>
2021-02-11 12:58:13 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,26 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            InMemoryDal inMemoryDal = new InMemoryDal();
+            CarManager carManager = new CarManager(inMemoryDal);
+            var x = carManager.GetAll();
+            foreach (var c in x)
+            {
+                Console.WriteLine(c.DailyPrice);
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
